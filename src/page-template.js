78b7949aff415d6414cf5f026d494ded778ece01
vutanguofa <vu.q.tang3@gmail.com>
2020-09-27 01:18:27 -0700

    updated questions section

--- src/page-template.js
+++ src/page-template.js
@@ -1,7 +1,7 @@
 // export function to generate entire page
 module.exports = templateData => {
   // destructure page data by section
-  const { projectName, desc, tableContents, install, usage, license, contrib, tests, questions } = templateData;
+  const { projectName, desc, tableContents, install, usage, license, contrib, tests, gitHub, email } = templateData;
 
   return `
     ${projectName}
@@ -28,7 +28,7 @@ module.exports = templateData => {
     ${tests}
 
     ## Questions
-    ${questions}
+    For questions, please reach out to me on github (${gitHub}) or via email (${email}).
 
   `;
 };
